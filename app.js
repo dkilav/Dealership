7cd99c966b52289a777d43176d93a283f8616816
dkilav <vlad5222vd@gmail.com>
2024-05-30 19:49:47 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+console.log('Ivan')
+console.log(444)
+ 
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:'class',
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'body':['Be Vietnam Pro'],
+      }
+    },
+  },
+  plugins: [],
+}
+
